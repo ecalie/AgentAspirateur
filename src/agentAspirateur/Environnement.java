e6fbcddf7c8940b04906df638680f2248b5e7e4f
ecalie <aurelie.forzani@gmail.com>
2018-09-23 21:51:42 -0400

    generation poussiere et bijou

--- src/agentAspirateur/Environnement.java
+++ src/agentAspirateur/Environnement.java
@@ -1,23 +1,54 @@
 package agentAspirateur;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Environnement {
 
-	private int longueur = 10;
-	private int largeur = 10;
+	private int longueur;
+	private int largeur;
 	private int mesurePerformance; 
+	private List<List<Piece>> pieces;	
+	
+	/**
+	 * Créer l'environnement.
+	 * @param longueur Nombre de pièces sur une ligne.
+	 * @param largeur Nombre de lignes de pièces.
+	 */
+	public Environnement(int longueur, int largeur) {
+		this.longueur = longueur;
+		this.largeur = largeur;
+		
+		this.pieces = new ArrayList<>();
+		for (int i = 0 ; i < this.largeur ; i++) {
+			List<Piece> lignePieces = new ArrayList<>();
+			for (int j = 0 ; j < this.longueur ; j++) {
+				lignePieces.add(new Piece(i,j));
+			}
+			this.pieces.add(lignePieces);
+		}
+		
+		this.mesurePerformance = 0;
+	}
 	
 	/**
 	 * Générer de la pousière dans une pièce aléatoire.
 	 */
 	public void genererPoussiere() {
+		int abscisse = (int) Math.random() * this.largeur;
+		int ordonnee = (int) Math.random() * this.longueur;
 		
+		this.pieces.get(ordonnee).get(abscisse).setPoussiere(true);
 	}
 	
 	/**
 	 * Générer un bijou dans une pièce aléatoire. 
 	 */
 	public void genererBijou() {
+		int abscisse = (int) Math.random() * this.largeur;
+		int ordonnee = (int) Math.random() * this.longueur;
 		
+		this.pieces.get(ordonnee).get(abscisse).setBijou(true);		
 	}
 	
 	/**
@@ -32,7 +63,11 @@ public class Environnement {
 	 */
 	public void demarrer() {
 		while (true) {
-			// Générer poussière ou bijou (selon une probabilité à définir)
+			double probabilité = Math.random();
+			if (probabilité > 0.66)
+				this.genererBijou();
+			else 
+				genererPoussiere();
 		}
 	}
 }
